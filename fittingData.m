eb7f62ee9faf040f11608009b1b6097304b7033e
hmoldenhauer <henning.moldenhauer@udo.edu>
2016-09-09 15:04:00 +0200

    removed overhead variables
    removed overhead variables

--- fittingData.m
+++ fittingData.m
@@ -21,15 +21,13 @@ function [f, gof, x, y] = fittingData(data, campx,...
                                       numberofgaussians, n)
 
 % define some helping variables
-limit = (n-1)*campx;
-lower = 1 + limit;
+lower = 1 + (n-1)*campx;
 upper = n*campx;
 
 % find starting parameters in given intervall
 x = data.XData(lower:upper);
-x1 = data.XData(lower:upper);
 y = data.YData(lower:upper);
-x_values = x>x_min & x1<x_max;      % define intervall
+x_values = x>x_min & x<x_max;      % define intervall
 x = x(x_values);                    % set x and
 y = y(x_values);                    % y values
 
@@ -37,7 +35,7 @@ y = y(x_values);                    % y values
 yy = smooth(y);                                         % smooth the data
 [pks, locs] = findpeaks(yy,'MinPeakProminence',0.4);    % find peaks
 [amplitude, index] = sortrows([pks, locs], -1);         % sort them to fit to highest peaks
-    
+
 % build fit function dynamically for the defined number of gaussians
 fitstring = 'y0+a*x';                           % use a linear function as base
 for k = 1:numberofgaussians
