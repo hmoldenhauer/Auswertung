0af167d665f8ec958f854881e48962eb0f3ac2b3
hmoldenhauer <henning.moldenhauer@udo.edu>
2016-09-12 17:34:17 +0200

    read all information to data

--- fittingData.m
+++ fittingData.m
@@ -36,29 +36,49 @@ y = y(x_values);                    % y values
 
 % find peaks in the given data
 yy = smooth(y);                                         % smooth the data
-[pks, locs] = findpeaks(yy,'MinPeakProminence',0.4);    % find peaks
-[amplitude, index] = sortrows([pks, locs], -1);         % sort them to fit to highest peaks
+% check if it is empty
+if isempty(yy)
+    fprintf('Ups, this is extremly smooth...\n');
+    f = 0;
+    gof = 0;
+    x = 0;
+    y = 0;
+    amplitudes = 0;
+    positions = 0;
+else
+    [pks, locs] = findpeaks(yy,'MinPeakProminence',0.4);    % find peaks
+    [amplitude, index] = sortrows([pks, locs], -1);         % sort them to fit to highest peaks
+    
+    fitstring = 'y0+a*x';                                   % use a linear function as base
 
-% build fit function dynamically for the defined number of gaussians
-fitstring = 'y0+a*x';                           % use a linear function as base
-for k = 1:numberofgaussians
-    ampstr = strcat('amp', num2str(k));
-    posstr = strcat('pos', num2str(k));
-    varstr = strcat('var', num2str(k));
-    gaussstr = strcat(ampstr, '*exp(-(x-', posstr, ')^2/(2*', varstr, '^2))');
-    fitstring = strcat(fitstring, '+', gaussstr);
-end
- 
-% build fittype which will be used for fitting
-multigaussfit = fittype(fitstring);
-
-% build fitting parameters
-amplitudes = amplitude(1:numberofgaussians);
-positions = x(locs(index(1:numberofgaussians))');
-variances = ones(1,numberofgaussians);
-
-% fitfunction -> if anything is changed, take care of the StartPoint!!!
-[f, gof] = fit(x',y',...
+    if isempty(amplitude)
+        amplitudes = [];
+        positions = [];
+        variances = [];
+        fprintf('No peaks at all.\n');
+    elseif size(amplitude) < numberofgaussians
+        numberofgaussians = size(amplitude);
+        fprintf('Only few peaks in this spectrum. Has been changed to %d\n.', numberofgaussians);
+    else
+        % build fit function dynamically for the defined number of gaussians
+        for k = 1:numberofgaussians
+            ampstr = strcat('amp', num2str(k));
+            posstr = strcat('pos', num2str(k));
+            varstr = strcat('var', num2str(k));
+            gaussstr = strcat(ampstr, '*exp(-(x-', posstr, ')^2/(2*', varstr, '^2))');
+            fitstring = strcat(fitstring, '+', gaussstr);
+        end
+        % build fitting parameters
+        amplitudes = amplitude(1:numberofgaussians);
+        positions = x(locs(index(1:numberofgaussians))');
+        variances = ones(1,numberofgaussians);
+    end
+        
+    % build fittype which will be used for fitting
+    multigaussfit = fittype(fitstring);
+    
+    % fitfunction -> if anything is changed, take care of the StartPoint!!!
+    [f, gof] = fit(x',y',...
                    multigaussfit,...
                    'StartPoint',...
                    [1,...
@@ -66,3 +86,4 @@ variances = ones(1,numberofgaussians);
                    positions,...
                    variances,...
                    0]);
+end
