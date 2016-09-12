0b30f6235190927f658704437ed5d97db267dcc7
hmoldenhauer <henning.moldenhauer@udo.edu>
2016-09-12 17:36:58 +0200

    minor changes

--- auswertung.m
+++ auswertung.m
@@ -24,58 +24,61 @@ addpath(genpath('..\Auswertung\'));
 % ----------------------------------------------------------------------
 
 % read the data
-%data = readData(datafolder, 'Temp-Freq.txt');
+data = readData(datafolder, 'details_matlab.txt');
 
 % ----------------------------------------------------------------------
 % Fitting the data
 % ----------------------------------------------------------------------
-for m  = 1:3    % data(end).Measurement
-% find out number of pixels of the CCD and the number of spectra
-[campx, spectra] = size(data(m).XData);
-
-% fit all the spectra
-for n = 1:spectra
-    [ftemp, goftemp, xtemp, ytemp, ampstemp, postemp] = fittingData(data(m),...
-                                                        campx,...
-                                                        x_min, x_max,...
-                                                        numberofgaussians, n);
-    % save fit data of all spectra
-    f{m,n} = ftemp;
-    gof{m,n} = goftemp;
-    x{m,n} = xtemp;
-    y{m,n} = ytemp;
-    amps{m,n} = ampstemp;
-    pos{m,n} = postemp;
-    fprintf('Fit %d of %d finished\n', n, spectra);
+for m = 1:data(end).Measurement
+    if data(m).PL == 1
+    % find out number of pixels of the CCD and the number of spectra
+    [campx, spectra] = size(data(m).XData);
     
-    % plot each 10th fit in a new figure
-    if mod(n,30) == 0 | n == 1
-        figure;
-        hold on;
-        plot(f{m,n},x{m,n},y{m,n});
-        plot(pos{m,n},amps{m,n}, 'ro');
-        
-        % plot seperate fit functions used
-        if true
-            for k = 1:numberofgaussians
-                if k == 1
-                    fplot(@(x) f{m,n}.('y0')+f{m,n}.('a')*x,...
-                        [x{m,n}(1),x{m,n}(end)]);
-                end
-                ampstr = strcat('amp', num2str(k));
-                posstr = strcat('pos', num2str(k));
-                varstr = strcat('var', num2str(k));
-                fplot(@(x) f{m,n}.(ampstr)*exp(-(x-f{m,n}.(posstr))^2/(2*f{m,n}.(varstr)^2)),...
-                    [x{m,n}(1),x{m,n}(end)]);
-            end
-        end
-        xlabel('Wavelength (nm)');
-        ylabel('Intensity (cps)');
-        title(['m = ' num2str(m) ' n = ' num2str(n)]);
-        hold off;
-        fprintf('Plot %d of %d finished\n', n, spectra);
+    % fit all the spectra
+    for n = 1:10:spectra
+        [ftemp, goftemp, xtemp, ytemp, ampstemp, postemp] = fittingData(data(m),...
+                                                            campx,...
+                                                            x_min, x_max,...
+                                                            numberofgaussians, n);
+         % save fit data of all spectra
+         f{m,n} = ftemp;
+         gof{m,n} = goftemp;
+         x{m,n} = xtemp;
+         y{m,n} = ytemp;
+         amps{m,n} = ampstemp;
+         pos{m,n} = postemp;
+         fprintf('Fit %d of %d finished\n', n, spectra);
+         
+         if xtemp == 0
+         % plot each 10th fit in a new figure
+         elseif mod(n,30) == 0 | n == 1
+             figure;
+             hold on;
+             plot(f{m,n},x{m,n},y{m,n});
+             plot(pos{m,n},amps{m,n}, 'ro');
+             
+             % plot seperate fit functions used
+             if true
+                 for k = 1:numberofgaussians
+                     if k == 1
+                         fplot(@(x) f{m,n}.('y0')+f{m,n}.('a')*x,...
+                             [x{m,n}(1),x{m,n}(end)]);
+                     end
+                     ampstr = strcat('amp', num2str(k));
+                     posstr = strcat('pos', num2str(k));
+                     varstr = strcat('var', num2str(k));
+                     fplot(@(x) f{m,n}.(ampstr)*exp(-(x-f{m,n}.(posstr))^2/(2*f{m,n}.(varstr)^2)),...
+                         [x{m,n}(1),x{m,n}(end)]);
+                 end
+             end
+             xlabel('Wavelength (nm)');
+             ylabel('Intensity (cps)');
+             title(['m = ' num2str(m) ' n = ' num2str(n)]);
+             hold off;
+             fprintf('Plot %d of %d finished\n', n, spectra);
+         end
     end
-end
+    end
 end
 
 clear -regexpr *temp *str spectra campx k m n
